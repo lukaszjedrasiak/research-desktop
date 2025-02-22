b9f301c2926230626f9ea083ffff5969152496e8
LukaszJedrasiak <lukasz@jedrasiak.eu>
2025-02-22 20:22:45 +0100

    feat(main): add graph validation

--- src/main/graph.js
+++ src/main/graph.js
@@ -1,17 +1,63 @@
 const { dialog } = require('electron');
 const fs = require('fs').promises;
+const path = require('path');
 
 let currentGraphPath = null;
 let currentGraphItems = null;
 
 // main function
 async function graphOpen() {
-    const path = await graphSelect();
-    if (!path) return;
+    const graphPath = await graphSelect();
+    console.log(`path: ${path}`);
+    console.log(`currentGraphPath: ${currentGraphPath}`);
+
+    if (!graphPath) return;
     
     // process the graph
-    const items = await graphRead(path);
-    console.log(items);
+    const graphItems = await graphRead(graphPath);
+    console.log(graphItems);
+
+    try {
+        if (!graphItems.includes('.research')) {
+            dialog.showMessageBox({
+                title: 'Warning',
+                message: 'The .research item is missing.',
+                type: 'warning'
+            });
+            return;
+        }
+
+        if (!(await isFolder('.research'))) {
+            dialog.showMessageBox({
+                title: 'Warning',
+                message: 'The .research item is not a folder.',
+                type: 'warning'
+            });
+            return;
+        }
+
+        const researchFolder = path.join(currentGraphPath, '.research');
+        const researchFolderItems = await fs.readdir(researchFolder);
+
+        if (!researchFolderItems.includes('graph.yaml')) {
+            dialog.showMessageBox({
+                title: 'Warning',
+                message: 'The graph.yaml file is missing.',
+                type: 'warning'
+            });
+            return;
+        }
+        
+
+    } catch (error) {
+        console.error('Error validating .research folder:', error);
+        dialog.showMessageBox({
+            title: 'Error',
+            message: 'Error validating folder structure',
+            type: 'error'
+        });
+        return;
+    }
 }
 
 // helpers
@@ -43,6 +89,17 @@ async function graphRead(path) {
         console.error('Error reading folder:', error);
         throw error;
     }
+}
+
+// validators
+function itemExists(items, itemName) {
+    return items.includes(itemName);
+}
+
+async function isFolder(item) {
+    const itemPath = path.join(currentGraphPath, item);
+    const itemStats = await fs.stat(itemPath);
+    return itemStats.isDirectory();
 }
 
 // getters
