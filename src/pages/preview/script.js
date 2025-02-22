2ba91508457b6aa0cfb4ee4718c660f18cfd20bb
LukaszJedrasiak <lukasz@jedrasiak.eu>
2025-02-22 23:45:48 +0100

    feat(preview): add fetching vertices scaffold

--- src/pages/preview/script.js
+++ src/pages/preview/script.js
@@ -2,6 +2,6 @@ console.log('# pages/preview/script.js');
 
 document.addEventListener('DOMContentLoaded', async () => {
     const graphData = await window.api_internal.getGraphData();
-    console.log(graphData);
-    console.log(graphData.path);
+    console.log(`%c# graphData`, 'color: lightblue');
+    console.log(`%c${JSON.stringify(graphData, null, 2)}`, 'color: plum');
 });
