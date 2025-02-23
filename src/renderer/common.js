082ec29bcd421e4faa0ab7a068d7ac8a1c926054
LukaszJedrasiak <lukasz@jedrasiak.eu>
2025-02-23 15:52:37 +0100

    style(preview): add layout scaffold

--- src/renderer/common.js
+++ src/renderer/common.js
@@ -2,7 +2,7 @@ console.log(`# renderer/common.js`);
 
 async function displayVersion() {
     const version = await window.api_internal.getAppVersion();
-    document.getElementById('app-version').innerText = `version: ${version}`;
+    document.getElementById('app-version').innerText = `${version}`;
 }
 
 displayVersion();
